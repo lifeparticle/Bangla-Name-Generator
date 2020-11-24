9bedda439c4b878013a62c8357dff3192be706ff
Mahbub Zaman <mahbubzaman17@gmail.com>
2020-11-24 18:23:16 +0600

    Update gen.py

--- api/gen.py
+++ api/gen.py
@@ -1,12 +1,13 @@
 from http.server import BaseHTTPRequestHandler
 from datetime import datetime
+from urlparse import parse_qs
 
 class handler(BaseHTTPRequestHandler):
 
   def do_GET(self):
     print (self.path)
-    print (self.request)
-    #print (self.request.get('gender'))
+    s = self.path
+    print parse_qs(s[2:])
     self.send_response(200)
     self.send_header('Content-type', 'text/plain')
     self.end_headers()
