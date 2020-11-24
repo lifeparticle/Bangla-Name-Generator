0fb104bf670d33e4f709a48e474cebe08cbcb05a
Mahbub Zaman <mahbubzaman17@gmail.com>
2020-11-24 18:14:57 +0600

    Update gen.py

--- api/gen.py
+++ api/gen.py
@@ -6,7 +6,7 @@ class handler(BaseHTTPRequestHandler):
   def do_GET(self):
     print (self)
     print (self.request)
-    print (self.request.get('gender'))
+    #print (self.request.get('gender'))
     self.send_response(200)
     self.send_header('Content-type', 'text/plain')
     self.end_headers()
