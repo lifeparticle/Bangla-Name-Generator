1b2d2269573d4dd69ee694236fe81512f9587002
Mahbub Zaman <mahbubzaman17@gmail.com>
2020-12-27 23:37:53 +1100

    Update gen.py

--- api/gen.py
+++ api/gen.py
@@ -4,47 +4,46 @@ import random as rand
 from urllib import parse
 
 class handler(BaseHTTPRequestHandler):
-    
-    def name_gen(self, key):
-          key = key.lower()
-          if key == 'male':
-            first_names = 'api/male_first_names.txt'
-          elif key == 'female':
-            first_names = 'api/female_first_names.txt'
-            
-          last_names = 'api/last_names.txt'
 
-          f = open(str(first_names), "r")
-          first_names = f.read().split()
+	def name_gen(self, key):
+		key = key.lower()
+		if key == 'male':
+			first_names = 'api/male_first_names.txt'
+		elif key == 'female':
+			first_names = 'api/female_first_names.txt'
 
-          f = open(str(last_names), "r")
-          last_names = f.read().split()
+		last_names = 'api/last_names.txt'
 
-          first_names_size = len(first_names)
-          last_names_size = len(last_names)
-          first_index = rand.randint(0, first_names_size)
-          last_index = rand.randint(0, last_names_size)
+		f = open(str(first_names), "r")
+		first_names = f.read().split()
 
-          name = first_names[first_index]+" "+last_names[last_index]
-          output = {key:name}
+		f = open(str(last_names), "r")
+		last_names = f.read().split()
 
-          json_value = json.dumps(output, ensure_ascii = False)
-          return json_value
-    
-    def do_GET(self):
-        s = self.path
+		first_names_size = len(first_names)
+		last_names_size = len(last_names)
+		first_index = rand.randint(0, first_names_size)
+		last_index = rand.randint(0, last_names_size)
+
+		name = first_names[first_index]+" "+last_names[last_index]
+		output = {key:name}
+
+		return output
+
+	def do_GET(self):
+		s = self.path
 		dic = dict(parse.parse_qsl(parse.urlsplit(s).query))
-        self.send_response(200)
-        self.send_header('Content-type','application/json; charset=utf-8')
-        self.send_header('Access-Control-Allow-Origin', '*')
-        self.end_headers()
-        
-        if "gender" in dic:
-            output = self.name_gen(dic["gender"])
-        else:
-            output = "param missing"
-            
-        json_value = json.dumps(output, ensure_ascii = False)
-        result = json.loads(json_value)
-        self.wfile.write(result.encode('utf8'))
-        return
+		self.send_response(200)
+		self.send_header('Content-type','application/json; charset=utf-8')
+		self.send_header('Access-Control-Allow-Origin', '*')
+		self.end_headers()
+
+		if "gender" in dic:
+			output = self.name_gen(dic["gender"])
+		else:
+			output = "param missing"
+
+		json_value = json.dumps(output, ensure_ascii = False)
+		result = json.loads(json_value)
+		self.wfile.write(result.encode('utf8'))
+		return
