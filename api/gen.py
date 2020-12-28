123cdef94078b46859c0f3cc4a57ca04c1e1485b
lifeparticle <mahbubzaman17@gmail.com>
2020-12-28 16:40:58 +1100

    rm headers

--- api/gen.py
+++ api/gen.py
@@ -44,10 +44,6 @@ class handler(BaseHTTPRequestHandler):
 			data['error'] = 'param missing'
 			output = json.dumps(data)
 
-		self.send_header('Content-type','application/json; charset=utf-8')
-		self.send_header('Access-Control-Allow-Origin', '*')
-		self.end_headers()
-
 		json_value = json.dumps(output, ensure_ascii = False)
 		result = json.loads(json_value)
 		self.wfile.write(result.encode('utf8'))
