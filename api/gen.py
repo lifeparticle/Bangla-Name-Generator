1090f765eabf40fd50c155ff49fa7e68d7ce2b67
lifeparticle <mahbubzaman17@gmail.com>
2021-01-08 19:32:43 +1100

    clean

--- api/gen.py
+++ api/gen.py
@@ -1,54 +1,54 @@
 from http.server import BaseHTTPRequestHandler
 from os.path import join
 from urllib import parse
-import random as rand
+import random
 import json
+
+MALE_FIRST_NAMES_FILE_PATH = 'data/male_first_names.txt'
+FEMALE_FIRST_NAMES_FILE_PATH = 'data/female_first_names.txt'
+LAST_NAMES_FILE_PATH = 'data/last_names.txt'
+MALE_KEY = 'male'
 
 class handler(BaseHTTPRequestHandler):
 
+	def read_file(self, file_name):
+		try:
+			f = open(file_name, "r")
+			return f.read().split()
+		except:
+  			print("Error")
+		finally:
+			f.close()
+
+	def random_name(self, list):
+		return random.choice(list)
+
 	def name_gen(self, key):
-		key = key.lower()
-		if key == 'male':
-			first_names = 'male_first_names.txt'
-		elif key == 'female':
-			first_names = 'female_first_names.txt'
+		first_names_file_path = MALE_FIRST_NAMES_FILE_PATH if key == MALE_KEY else FEMALE_FIRST_NAMES_FILE_PATH
+		last_names_file_path = LAST_NAMES_FILE_PATH
 
-		last_names = 'last_names.txt'
+		first_names = self.read_file(first_names_file_path)
+		last_names = self.read_file(last_names_file_path)
 
-		f = open(join('data', first_names), "r")
-		first_names = f.read().split()
-
-		f = open(join('data', last_names), "r")
-		last_names = f.read().split()
-
-		first_names_size = len(first_names)
-		last_names_size = len(last_names)
-		first_index = rand.randint(0, first_names_size)
-		last_index = rand.randint(0, last_names_size)
-
-		name = first_names[first_index]+" "+last_names[last_index]
-		output = {key : name}
-
-		return json.dumps(output, ensure_ascii=False)
+		return "{first_name} {last_name}".format(first_name = self.random_name(first_names), last_name = self.random_name(last_names))
 
 	def do_GET(self):
-		s = self.path
-		dic = dict(parse.parse_qsl(parse.urlsplit(s).query))
+		dic = dict(parse.parse_qsl(parse.urlsplit(self.path).query))
 
 		if "gender" in dic:
 			self.send_response(200)
-			output = self.name_gen(dic["gender"])
+			key = dic["gender"].lower()
+			data = {key: self.name_gen(key)}
 		else:
 			self.send_response(404)
-			data = {}
-			data['error'] = 'param missing'
-			output = json.dumps(data)
+			data = {"error": "param missing"}
 
 		self.send_header('Content-type','application/json; charset=utf-8')
 		self.send_header('Access-Control-Allow-Origin', '*')
 		self.end_headers()
 
-		json_value = json.dumps(output, ensure_ascii = False)
+		data = json.dumps(data, ensure_ascii = False)
+		json_value = json.dumps(data, ensure_ascii = False)
 		result = json.loads(json_value)
 		self.wfile.write(result.encode('utf8'))
 		return
